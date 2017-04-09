0dc6dcc06cc205452cb758edcce7bd584361e976
Jarred Capellman <jcapellman@hotmail.com>
2017-04-08 21:39:52 -0500

    Initial commit

---
+++ src/pee/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace pee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No filename given, exiting");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"File {args[0]} does not exist");
+                return;
+            }
+
+            var analyzer = new PeeAnalyzer(args[0]);
+
+            analyzer.Analyze();
+
+            Console.WriteLine($"Analysis: {analyzer}");
+        }
+    }
+}
---
+++ src/pee/peeanalyzer.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace pee
+{
+    public class PeeAnalyzer
+    {
+        private readonly string _fileName;
+
+        private string _str;    
+
+        public PeeAnalyzer(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void Analyze()
+        {
+            var text = File.ReadAllText(_fileName, Encoding.ASCII);
+            var matches = Regex.Matches(text, @"([a-zA-Z])");
+
+            foreach (var match in matches)
+            {
+                _str += $"{match}{System.Environment.NewLine}";
+            }
+        }
+
+        public override string ToString()
+        {
+            return _str;
+        }
+    }
+}
